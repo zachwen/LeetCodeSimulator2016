70b17a9147e2d6604cb52af61fd76bb0b3016a79
Zhaoli Wen <zwen6@illinois.edu>
2016-08-19 15:19:52 -0500

    setup dev env

---
+++ leetcode.h
@@ -0,0 +1,20 @@
+#ifndef LEETCODE_H
+#define LEETCODE_H
+
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+#endif
